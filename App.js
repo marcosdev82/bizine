6402b472230d46e8aeb8bb29568c0f9cc3793f4d
mikek182 <marcosvinicius@geekwork.com.br>
2024-07-16 22:01:54 -0300

    Criação do formulário e formatação da página SignIn

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import logo from '../bizine/src/assets/logo.png';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        <StatusBar barStyle="dark-content" translucent={true} backgroundColor='#f1f1f1'/>
+
+        <Image source={logo} style={styles.logo} />
+
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <Text style={styles.title}>Acesse sua conta</Text>
+            <TextInput placeholderTextColor="#555" placeholder='Email' style={styles.input}/>
+            <TextInput placeholderTextColor="#555" placeholder='senha' style={styles.input}/>
+          </View>
+
+          <TouchableOpacity style={styles.buttonSignIn}>
+            <Text style={styles.textButton}>Entrar</Text>
+          </TouchableOpacity>
+        </View>
+
     </View>
   );
 }
@@ -14,8 +31,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f9db14',
+    justifyContent: 'start',
+    flexDirection: 'column',
+    alignItems: 'center', 
   },
+  content: {
+    width: '90%',
+  },
+  logo: {
+    marginTop: 100,
+    marginBottom: 100,
+  },
+  form: {
+    width: '100%',
+    backgroundColor: '#fff',
+    padding: 30,
+    paddingTop: 50,
+    paddingBottom: 50,
+    borderRadius: 8,
+    gap: 15,
+  },
+  input: {
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    padding: 10,
+  },
+  title: {
+    fontSize: 22,
+    marginBottom: 30,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+    textAlign: 'center',
+    fontWeight: '600'
+  },
+  buttonSignIn: {
+    backgroundColor: '#18161a',
+    padding: 15,
+    borderRadius: 25,
+    alignItems: 'center',
+    marginTop: 10,
+    width: '100%',
+  },
+  textButton: {
+    color: '#fff'
+  }
+
 });
